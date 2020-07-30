c701fa6cfb9052f44664b5339220cc2a13a935fc
Angela Thomas <angeladthomas97@gmail.com>
2020-07-30 15:34:23 -0500

    Didn't have yesteradys updates

--- src/NoteListNav/NoteListNav.js
+++ src/NoteListNav/NoteListNav.js
@@ -2,43 +2,53 @@ import React from 'react'
 import { NavLink, Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import CircleButton from '../CircleButton/CircleButton'
+import ApiContext from '../ApiContext'
 import { countNotesForFolder } from '../notes-helpers'
-import './NoteListNav.css'
+import './NoteListNav.css';
+import AddFolder from '../AddFolder/AddFolder';
 
-export default function NoteListNav(props) {
-  return (
-    <div className='NoteListNav'>
-      <ul className='NoteListNav__list'>
-        {props.folders.map(folder =>
-          <li key={folder.id}>
-            <NavLink
-              className='NoteListNav__folder-link'
-              to={`/folder/${folder.id}`}
+export default class NoteListNav extends React.Component {
+  static contextType = ApiContext;
+
+  render() {
+    const { folders=[], notes=[] } = this.context
+    return (
+      <div className='NoteListNav'>
+        <ul className='NoteListNav__list'>
+          {folders.map(folder =>
+            <li key={folder.id}>
+              <NavLink
+                className='NoteListNav__folder-link'
+                to={`/folder/${folder.id}`}
+              >
+                <span className='NoteListNav__num-notes'>
+                  {countNotesForFolder(notes, folder.id)}
+                </span>
+                {folder.name}
+              </NavLink>
+            </li>
+          )}
+        </ul>
+        <div className='NoteListNav__button-wrapper'>
+          <CircleButton
+            tag={Link}
+            to='/add-folder'
+            type='button'
+            className='NoteListNav__add-folder-button'              
+          >
+            <FontAwesomeIcon icon='plus' />
+            <br />
+            Folder
+          </CircleButton>
+          <div className="NewFolderControl">
+            <AddFolder
+              tag='button'
+              role='link'
+              onClick={() => this.props.folders[].push()}
             >
-              <span className='NoteListNav__num-notes'>
-                {countNotesForFolder(props.notes, folder.id)}
-              </span>
-              {folder.name}
-            </NavLink>
-          </li>
-        )}
-      </ul>
-      <div className='NoteListNav__button-wrapper'>
-        <CircleButton
-          tag={Link}
-          to='/add-folder'
-          type='button'
-          className='NoteListNav__add-folder-button'
-        >
-          <FontAwesomeIcon icon='plus' />
-          <br />
-          Folder
-        </CircleButton>
+            </AddFolder>
+        </div>
       </div>
-    </div>
-  )
-}
-
-NoteListNav.defaultProps = {
-  folders: []
+    )
+  }
 }
