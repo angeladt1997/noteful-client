2d59b03dd63cd58f82a14b5a651a6407b4a3e59a
Angela Thomas <angeladthomas97@gmail.com>
2020-07-30 21:10:48 -0500

    angelas latest updates

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,9 +1,3 @@
-// Create a new component AddFolder that implements a form to capture 
-// the name of a new folder from the user. This form should submit the 
-// name of the new folder to the POST /folders endpoint on the server. 
-// Ensure that any errors are properly handled. Add a button to the 
-// navigation to invoke the new form.
-
 import React from 'react';
 import './AddFolder.css';
 import config from '../config';
@@ -83,15 +77,4 @@ export default class AddFolder extends React.Component {
 	}
 
 
-	//addNewFolder(newFolder) {
-		//	this.setState({newFolder : {value: newFolder}});
-		//}
-		// handleSubmit(event) {
-		// 	event.preventDefault();
-		// 	const newFolder = event.folderEntryInput.current.value;
-		// }
-
-		// handleDelete(eventTwo) {
-		// 	event.preventDefault();
-		// 	const noNewFolder = event.
-		// }
+	
